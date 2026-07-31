df00942134b532be1001c4294c9e5c9ffa8f416f
Erliandikasyahputraa <syahputraerliandika@gmail.com>
2026-08-01 02:11:03 +0700

    fix(home): remove unused imports in composition

--- src/features/home/HomeComposition.tsx
+++ src/features/home/HomeComposition.tsx
@@ -1,11 +1,13 @@
 import { HeroSection } from './sections/Hero';
 import { StorySection } from './sections/Story';
 import { NatureSection } from './sections/Nature';
+/* 
 import { PotentialsSection } from './sections/Potentials';
 import { TimelineSection } from './sections/Timeline';
 import { GallerySection } from './sections/Gallery';
 import { TransparencySection } from './sections/Transparency';
 import { CTASection } from './sections/CTA';
+*/
 
 export function HomeComposition() {
   return (
